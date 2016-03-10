3deb665c13902767f65d7c8c5a2623ab52917bad
Nitin kumar <knitin917@gmail.com>
2016-03-10 05:59:33 +0530

    third commit

--- app/routes/main.js
+++ app/routes/main.js
@@ -4,43 +4,42 @@ var variantA = require('../controllers/variantA.server.controller');
 var variantB = require('../controllers/variantB.server.controller');
 module.exports = function(app) {
 	
-	app.use(function (req, res) {
-	  // check if client sent cookie
+	app.use(function (req, res, next) {
 	  var cookie = req.cookies.cookieName;
 
 	  if (cookie === undefined)
 	  {
-	    
-
 	    abSchema.findOne({}, function(err, query) {
 	  	if(query.flag==0)
  	  	{
-		//load page b
 	  	var page = query.variant[1];
-	  	//update flag
+	  	query.flag=1;
+	  	var a = query.total[1] +1;
+	  	var b = query.total[0];
+	  	query.total = [b,a];
 	  	res.cookie('cookieName',page, { maxAge: 900000, httpOnly: true });
 	    console.log('cookie created successfully');
 	  	res.sendFile('/home/nitin/Documents/MainStream Projects/meanhacknsit/public/variantB.html');
 	  	}
-	 	else
+	 	else		
 		{
 	  	var page = query.variant[0];
-	  	console.log(page);
-	  	//update flag
+	  	query.flag=0;
+	  	var a = query.total[0] +1;
+	  	var b = query.total[1];
+	  	query.total = [a,b];
 	  	res.cookie('cookieName',page, { maxAge: 900000, httpOnly: true });
 	    console.log('cookie created successfully');
 	  	res.sendFile('/home/nitin/Documents/MainStream Projects/meanhacknsit/public/variantA.html');
 	    }
-	  
+	  	query.save();
 	  });
 	  } 
 	  else
 	  {
-	  	app.get('/home', function(req, res) {
-  			console.log("Cookies: ", req.cookies)
-		});
+  			var page = '/home/nitin/Documents/MainStream Projects/meanhacknsit/public/' + req.cookies.cookieName + '.html'; 
+  			res.sendFile(page);
 	  }
 	});
-	
 };
 
